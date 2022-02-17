dfe8c2b26ea3b2685b0a4001f9af3ae756e7e4a7
woywro <woywro@gmail.com>
2022-02-17 19:21:43 +0100

    feat: add homepage tiles

--- src/views/HomePage/components/List/index.tsx
+++ src/views/HomePage/components/List/index.tsx
@@ -4,18 +4,21 @@ import { useSelector } from "react-redux";
 
 const StyledList = styled.ul`
   display: grid;
-  grid-template-columns: 1fr 1fr 1fr;
-  width:100%:
+  grid-template-columns: 1fr 1fr 1fr 1fr;
+  grid-template-rows: 1fr 1fr 1fr 1fr;
+  width: 100%;
   height: 100%;
   gap: 20px;
 `;
 export const List = () => {
   const Links = useSelector((state) => state.links);
+
   return (
     <StyledList>
-      {Links.map((e) => {
-        return <ListItem link={e} />;
-      })}
+      <ListItem link={"e"} title={"history"} col={"1"} row={"1/4"} bg="3" />
+      <ListItem link={"e"} title={"ddd"} col={"2/4"} row={"1/3"} bg="2" />
+      <ListItem link={"e"} title={"aaa"} col={"4/5"} row={"1"} bg="4" />
+      <ListItem link={"e"} title={"ccc"} col={"4/5"} row={"2"} bg="5" />
     </StyledList>
   );
 };
--- src/views/HomePage/components/ListItem/index.tsx
+++ src/views/HomePage/components/ListItem/index.tsx
@@ -1,26 +1,39 @@
 import styled from "styled-components";
-import { LinkInterface } from "../../../../types/LinkInterface";
+import { Text } from "../../../../components/Text";
 
 const StyledListItem = styled.li`
   display: flex;
   justify-content: center;
-  align-items: center;
+  align-items: flex-start;
   padding: 20px;
   border-radius: 20px;
-  background: ${(props) => props.color};
+  height: 100%;
   width: 100%;
-  height: 100px;
+  background: ${(props) => props.bg};
+  grid-column: ${(props) => props.column};
+  grid-row: ${(props) => props.row};
 `;
 
 interface Props {
-  link: LinkInterface;
+  title: string;
+  col: string;
+  row: string;
+  bg: string;
 }
 
-export const ListItem = ({ link }: Props) => {
-  const { name, url, color, tags } = link;
+export const ListItem = ({ title, col, row, bg }: Props) => {
+  const backgrounds = [
+    "white",
+    "radial-gradient(#76b2fe, #b69efe)",
+    "radial-gradient(#fbc1cc, #fa99b2)",
+    "radial-gradient(#1fe4f5, #3fbafe)",
+    "radial-gradient(#f588d8, #c0a3e5)",
+    "radial-gradient(#60efbc, #58d5c9)",
+  ];
+
   return (
-    <StyledListItem color={color}>
-      <p>{name}</p>
+    <StyledListItem column={col} row={row} bg={backgrounds[bg]}>
+      <Text>{title}</Text>
     </StyledListItem>
   );
 };
