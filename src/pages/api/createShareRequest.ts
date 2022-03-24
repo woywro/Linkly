b1bf1c885251be454d6e3300aabe2e2ca943f683
woywro <woywro@gmail.com>
2022-03-24 19:47:10 +0100

    fix: small bug fixes

--- src/pages/api/createShareRequest.ts
+++ src/pages/api/createShareRequest.ts
@@ -1,12 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
+import { PrismaClientKnownRequestError } from "@prisma/client/runtime";
 import { prisma } from "../../../prisma/PrismaClient";
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
   const data = req.body;
   const session = await getSession({ req });
-  console.log(data);
   try {
     const result = await prisma.ShareRequest.create({
       data: {
@@ -20,6 +20,16 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     });
     res.status(200).json(result);
   } catch (err) {
-    res.status(403).json({ err });
+    if (err instanceof PrismaClientKnownRequestError) {
+      if (err.code === "P2025") {
+        res.status(500).json("This user does not exist");
+      }
+      if (err.code === "P2002") {
+        res
+          .status(500)
+          .json("This collection is already shared with this user");
+      }
+      console.log(err);
+    }
   }
 };
