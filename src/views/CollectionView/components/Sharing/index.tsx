61fc6d51189574da76e165b228e17cfc2ee85da2
woywro <woywro@gmail.com>
2022-03-25 20:42:55 +0100

    feat: add email validation for sharing collection

--- src/views/CollectionView/components/Sharing/index.tsx
+++ src/views/CollectionView/components/Sharing/index.tsx
@@ -5,13 +5,12 @@ import { useDispatch } from "react-redux";
 import { Button } from "../../../../components/Button";
 import { EmptyState } from "../../../../components/EmptyState";
 import { CollectionInterface } from "../../../../types/CollectionInterface";
-import {
-  AddWrapper,
-  SharingWrapper,
-  SharedEmail,
-  SharedList,
-  StyledInput,
-} from "./style";
+import { AddWrapper, SharingWrapper, SharedEmail, SharedList } from "./style";
+import { Field, Form, Formik } from "formik";
+import * as Yup from "yup";
+import styled from "styled-components";
+import { Text } from "../../../../components/Text";
+import { InputStyling } from "../../../../components/Input";
 
 interface Props {
   collection: CollectionInterface;
@@ -19,7 +18,7 @@ interface Props {
 
 export const Sharing = ({ collection }: Props) => {
   const router = useRouter();
-  const [input, setInput] = useState("");
+  // const [input, setInput] = useState("");
   const [sharedList, setSharedList] = useState([]);
   const dispatch = useDispatch();
 
@@ -36,23 +35,26 @@ export const Sharing = ({ collection }: Props) => {
     }
   }, [collection, router]);
 
-  const handleAdd = useCallback(() => {
-    axios
-      .post("/api/createShareRequest", {
-        collectionId: router.query.collectionId,
-        email: input,
-      })
-      .then(() => {
-        setSharedList([
-          ...sharedList,
-          { email: input, collectionId: router.query.collectionId },
-        ]);
-      })
-      .catch((err) => {
-        alert(err.response.data);
-      });
-    setInput("");
-  }, [router, input, sharedList]);
+  const handleAdd = useCallback(
+    (email) => {
+      axios
+        .post("/api/createShareRequest", {
+          collectionId: router.query.collectionId,
+          email: email,
+        })
+        .then(() => {
+          setSharedList([
+            ...sharedList,
+            { email: email, collectionId: router.query.collectionId },
+          ]);
+        })
+        .catch((err) => {
+          alert(err.response.data);
+        });
+      // setInput("");
+    },
+    [router, sharedList]
+  );
 
   const handleDelete = (e) => {
     axios.post("/api/deleteShareRequest", {
@@ -61,14 +63,42 @@ export const Sharing = ({ collection }: Props) => {
     });
     setSharedList(sharedList.filter((x) => x.email !== e));
   };
+
+  const validationSchema = Yup.object({
+    email: Yup.string()
+      .email()
+      .min(3, "email is too short!")
+      .required("email is required"),
+  });
+
   return (
     <SharingWrapper>
       <AddWrapper>
-        <StyledInput
+        <Formik
+          initialValues={{
+            email: "",
+          }}
+          onSubmit={(values) => {
+            handleAdd(values.email);
+          }}
+          validationSchema={validationSchema}
+        >
+          {/* <StyledInput
           placeholder="email"
           onChange={(e) => setInput(e.target.value)}
-        />
-        <Button onClick={handleAdd}>add</Button>
+        /> */}
+          {({ errors, touched }) => (
+            <StyledForm>
+              <InputWrapper>
+                <StyledInput name="email" placeholder="email" />
+                {errors.email && touched.email ? (
+                  <Error>{errors.email}</Error>
+                ) : null}
+              </InputWrapper>
+              <Button type="submit">Add</Button>
+            </StyledForm>
+          )}
+        </Formik>
       </AddWrapper>
       <SharedList>
         {sharedList.length > 0 ? (
@@ -89,3 +119,34 @@ export const Sharing = ({ collection }: Props) => {
     </SharingWrapper>
   );
 };
+
+const StyledForm = styled(Form)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-flow: row;
+  width: 100%;
+  padding: 5px;
+`;
+
+const InputWrapper = styled.div`
+  display: flex;
+  flex-flow: column;
+  width: 100%;
+  position: relative;
+`;
+
+const Error = styled(Text)`
+  color: ${(props) => props.theme.colors.primary};
+  font-size: 12px;
+  padding: 5px;
+  position: absolute;
+  top: 100%;
+`;
+
+const StyledInput = styled(Field)`
+  ${InputStyling}
+  margin: 0;
+  padding: 10px;
+  background: ${(props) => props.theme.colors.primaryBg};
+`;
