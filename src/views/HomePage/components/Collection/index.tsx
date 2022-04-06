f67599e34b274ea22b1954fddc1f478122d39318
woywro <woywro@gmail.com>
2022-04-06 19:34:50 +0200

    fix: sortingMode typing

--- src/views/HomePage/components/Collection/index.tsx
+++ src/views/HomePage/components/Collection/index.tsx
@@ -12,7 +12,7 @@ interface Props {
   id: string;
   shareRequests: ShareRequestInterface[];
   item: any;
-  sortingMode: (arg: boolean) => void;
+  sortingMode: boolean;
 }
 
 export const Collection = ({
