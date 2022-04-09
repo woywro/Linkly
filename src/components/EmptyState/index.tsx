c77d589b7ed8a31d5a3e02a62e997ad65c7ba171
woywro <woywro@gmail.com>
2022-04-09 21:09:25 +0200

    feat: add action button for empty state

--- src/components/EmptyState/index.tsx
+++ src/components/EmptyState/index.tsx
@@ -1,13 +1,19 @@
+import { Button } from "../Button";
 import { Text } from "../Text";
 import { EmptyStateWrapper } from "./style";
 interface Props {
   msg: String;
+  buttonText?: String;
+  onClick?: () => void;
 }
 
-export const EmptyState = ({ msg }: Props) => {
+export const EmptyState = ({ msg, buttonText, onClick }: Props) => {
   return (
     <EmptyStateWrapper>
       <Text>{msg}</Text>
+      {buttonText !== undefined && (
+        <Button onClick={onClick}>{buttonText}</Button>
+      )}
     </EmptyStateWrapper>
   );
 };
