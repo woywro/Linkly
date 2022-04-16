40917407b40d005ab37e6d22af4ad20a83bf45bc
woywro <woywro@gmail.com>
2022-04-16 13:10:38 +0200

    fix: pwa not working

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const withPWA = require("next-pwa");
 module.exports = withPWA({
   pwa: {
     dest: "public",
+    register: true,
+    skipWaiting: true,
   },
   typescript: {
     ignoreBuildErrors: true,
