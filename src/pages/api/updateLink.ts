a4b2aeda4e5dab4e3e6e39aabf89c6877515da25
woywro <woywro@gmail.com>
2022-04-25 14:12:03 +0200

    refactor: small fixes

--- src/pages/api/updateLink.ts
+++ src/pages/api/updateLink.ts
@@ -2,58 +2,60 @@ import { NextApiRequest, NextApiResponse } from 'next';
 import { getSession, withApiAuthRequired } from '@auth0/nextjs-auth0';
 import { prisma } from '../../../prisma/PrismaClient';
 
-export default withApiAuthRequired(async (req: NextApiRequest, res: NextApiResponse) => {
-  const data = req.body;
-  const session = await getSession(req,res);
+export default withApiAuthRequired(
+  async (req: NextApiRequest, res: NextApiResponse) => {
+    const data = req.body;
+    const session = await getSession(req, res);
 
-  try {
-    const result = await prisma.Link.update({
-      where: { id: data.id },
-      data: {
-        title: data.title,
-        url: data.url,
-        owner: {session?.user?.email,
-        modificationTimestamp: Date.now().toString(),
-        collections: {
-          set: [],
-          connectOrCreate: data.collectionValues.map(
-            (collectionValue: string) => ({
-              create: {
-                value: collectionValue,
-                valId: `${session?.user?.email}/${collectionValue}`,
-                modificationTimestamp: Date.now().toString(),
-                color: '',
-                owner: session?.user?.email,
-              },
-              where: {
-                valId: `${session?.user?.email}/${collectionValue}`,
-              },
-            })
-          ),
-        },
-      },
-      select: {
-        id: true,
-        title: true,
-        url: true,
-        collections: {
-          select: {
-            id: true,
-            owner: true,
-            value: true,
-            links: true,
-            modificationTimestamp: true,
-            color: true,
-            shareRequests: true,
+    try {
+      const result = await prisma.Link.update({
+        where: { id: data.id },
+        data: {
+          title: data.title,
+          url: data.url,
+          owner: session?.user?.email,
+          modificationTimestamp: Date.now().toString(),
+          collections: {
+            set: [],
+            connectOrCreate: data.collectionValues.map(
+              (collectionValue: string) => ({
+                create: {
+                  value: collectionValue,
+                  valId: `${session?.user?.email}/${collectionValue}`,
+                  modificationTimestamp: Date.now().toString(),
+                  color: '',
+                  owner: session?.user?.email,
+                },
+                where: {
+                  valId: `${session?.user?.email}/${collectionValue}`,
+                },
+              })
+            ),
           },
         },
-        ownerId: true,
-        owner: true,
-        modificationTimestamp: true,
-      },
-    });
-    res.status(200).json(result);
-  } catch (err) {
-    res.status(403).json({ err });
+        select: {
+          id: true,
+          title: true,
+          url: true,
+          collections: {
+            select: {
+              id: true,
+              owner: true,
+              value: true,
+              links: true,
+              modificationTimestamp: true,
+              color: true,
+              shareRequests: true,
+            },
+          },
+          ownerId: true,
+          owner: true,
+          modificationTimestamp: true,
+        },
+      });
+      res.status(200).json(result);
+    } catch (err) {
+      res.status(403).json({ err });
+    }
   }
-});
+);
