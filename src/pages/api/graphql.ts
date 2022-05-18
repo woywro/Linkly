6e449a967242e46e9f79d07981aca3875a05a9b2
woywro <woywro@gmail.com>
2022-05-18 09:51:25 +0200

    initial setup

---
+++ src/pages/api/graphql.ts
@@ -0,0 +1,33 @@
+import { ApolloServer } from 'apollo-server-micro';
+import { typeDefs } from '../../../graphql/schema';
+import { resolvers } from '../../../graphql/resolvers';
+import Cors from 'micro-cors';
+import { createContext } from '../../../graphql/context';
+
+const cors = Cors();
+
+const apolloServer = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: createContext,
+});
+
+const startServer = apolloServer.start();
+
+export default cors(async function handler(req, res) {
+  if (req.method === 'OPTIONS') {
+    res.end();
+    return false;
+  }
+  await startServer;
+
+  await apolloServer.createHandler({
+    path: '/api/graphql',
+  })(req, res);
+});
+
+export const config = {
+  api: {
+    bodyParser: false,
+  },
+};
